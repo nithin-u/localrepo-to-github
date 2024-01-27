d1d7d0c36233059e21bb4f906d8b54ec1658400c
nithin-u <nitinu522@gmail.com>
2024-01-27 16:14:10 +0530

    Added the operators

---
+++ operators.java
@@ -0,0 +1,9 @@
+class operators {
+    public static void main(String[] args) {
+        System.out.println(10 + 20);
+        System.out.println(100 - 20);
+        System.out.println(10 * 20);
+        System.out.println(100 / 20);
+        System.out.println(10 % 2);
+    }
+}
