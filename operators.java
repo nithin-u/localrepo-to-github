f28f40cd5bff1384725ccf9e3d174d085cdfb497
nithin-u <nitinu522@gmail.com>
2024-01-27 16:37:41 +0530

    add initial files

--- operators.java
+++
@@ -1,9 +0,0 @@
-class operators {
-    public static void main(String[] args) {
-        System.out.println(10 + 20);
-        System.out.println(100 - 20);
-        System.out.println(10 * 20);
-        System.out.println(100 / 20);
-        System.out.println(10 % 2);
-    }
-}
